21ae47fe6f339047ce12fa1a99d39f785d175500
gautamsuranipromact <gautamsurani@promactinfo.com>
2022-03-15 14:01:28 +0530

    TestFairy

---
+++ app/src/main/java/com/example/demo/MyApp.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import android.app.Application
+import com.testfairy.TestFairy
+
+class MyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        TestFairy.begin(this, "SDK-DxnP94AH");
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: MyApp
+            private set
+    }
+}
